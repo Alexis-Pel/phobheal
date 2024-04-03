f861c746206f1986338d9b117079b1668293eae3
Infernaton <lubin.pauly@gmail.com>
2024-04-03 10:27:24 +0200

    Thalassophobia - add max Height

--- Assets/Scripts/Thalassophobia/Cage.cs
+++ Assets/Scripts/Thalassophobia/Cage.cs
@@ -7,10 +7,17 @@ public class Cage : MonoBehaviour
 {
     [SerializeField] private float offsetHeight;
     private float modifier;
+    private float maxHeight;
     // Update is called once per frame
+
+    private void Start()
+    {
+        maxHeight = transform.position.y;
+    }
     void Update()
     {
-        transform.SetPositionY(transform.position.y + GetOffset());
+        float newHeight = transform.position.y + GetOffset();
+        transform.SetPositionY(Mathf.Min(newHeight, maxHeight));
     }
 
     public void SetModifier(float modifier)
