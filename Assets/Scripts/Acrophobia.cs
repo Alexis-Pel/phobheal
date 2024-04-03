b6375dc9cdf139243cc3aee3ec6fa3178fc4ef99
alexis.pelissier <alexis.pelissier@edu.itescia.fr>
2024-04-03 13:29:06 +0200

    final to test

--- Assets/Scripts/Acrophobia.cs
+++ Assets/Scripts/Acrophobia.cs
@@ -8,22 +8,27 @@ public class Acrophobia : MonoBehaviour
     private Vector3 secondStepPosition;
 
     private GameObject player;
+    private GameObject gameManager;
     private int step = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        player = GameObject.FindGameObjectsWithTag("Player")[0];
-        // TODO: Override All functions
+        // player = GameObject.FindGameObjectsWithTag("Player")[0];
+        gameManager = GameObject.FindGameObjectsWithTag("GameController")[0];
     }
 
     public void WinStep()
     {
-        player.transform.position = secondStepPosition;
         step++;
         switch (step)
         {
             case 2:
+                break;
+            case 3:
+                gameManager.GetComponent<GameManager>().WinGame();
+                break;
+            default:
                 break;
         }
     }
