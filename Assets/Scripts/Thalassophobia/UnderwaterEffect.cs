7c12141a46a5ca2c5262e73740b06a78a746079b
Infernaton <lubin.pauly@gmail.com>
2024-04-10 09:59:35 +0200

    Thalassophobie - changing fog color during descend

--- Assets/Scripts/Thalassophobia/UnderwaterEffect.cs
+++ Assets/Scripts/Thalassophobia/UnderwaterEffect.cs
@@ -8,32 +8,31 @@ public class UnderwaterEffect : MonoBehaviour
     [SerializeField] private Material skyboxUnderwater;
     [SerializeField] private Material skyboxDeepUnderwater;
 
+    [SerializeField] private Color startDepthFogColor;
+    [SerializeField] private Color endDepthFogColor;
+
     [SerializeField] private float maxDepthSunlight;
 
-    private float maxExposure;
-    private float waterNivelY = 0.35f;
+    private readonly float _waterNivelY = 0.35f;
 
-    private void Start()
-    {
-        maxExposure = 1;
-    }
     private void Update()
     {
         float currentDepth = mainCamera.transform.position.y;
-        RenderSettings.fog = currentDepth < waterNivelY;
+        RenderSettings.fog = currentDepth < _waterNivelY;
 
         UpdateSkybox(currentDepth);
     }
 
     private void UpdateSkybox(float currentDepth)
     {
-        if (currentDepth < waterNivelY)
+        if (currentDepth < _waterNivelY)
         {
             float percentageDepth = Mathf.Abs(currentDepth) / Mathf.Abs(maxDepthSunlight);
             // Need to re-Instanciate another Materiel, or else it will rewrite the data on the reference one
             Material dummySkybox = Instantiate(skyboxUnderwater);
             dummySkybox.Lerp(skyboxUnderwater, skyboxDeepUnderwater, percentageDepth);
             RenderSettings.skybox = dummySkybox;
+            RenderSettings.fogColor = Color.Lerp(startDepthFogColor, endDepthFogColor, percentageDepth);
         } else
         {
             RenderSettings.skybox = skyboxSurface;
