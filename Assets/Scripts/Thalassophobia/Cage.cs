c77bba27874d9042eb6569ec1c09035cca1e4b9c
Infernaton <lubin.pauly@gmail.com>
2024-04-10 12:04:54 +0200

    Thalassophobia - add more flexibility about the movement of the cage
    It might be used in other scene so, an update is necessary

--- Assets/Scripts/Thalassophobia/Cage.cs
+++ Assets/Scripts/Thalassophobia/Cage.cs
@@ -3,31 +3,38 @@ using UnityEngine;
 public class Cage : MonoBehaviour
 {
     [SerializeField] private float offsetHeight;
+
+    // Will be remove once the change of difficulties is finished
+    [SerializeField] private float heightStart;
+    [SerializeField] private float heightGoal;
+
     private float modifier;
-    private float maxHeight;
-
-    [SerializeField] private float depthGoal; // Will be remove once the change of difficulties is finished
-
-    private void Start()
-    {
-        maxHeight = transform.position.y;
-    }
-    void Update()
-    {
-        float newHeight = transform.position.y + GetOffset();
-        transform.position = new Vector3(transform.position.x, Mathf.Max(depthGoal, Mathf.Min(newHeight, maxHeight)), transform.position.z);
-        if (transform.position.y == depthGoal)
-        {
-            GameManager.Instance.WinGame();
-        }
-    }
+    private bool _isDiving;
 
     public void SetModifier(float modifier)
     {
         this.modifier = modifier;
     }
 
-    public float GetOffset()
+    private void Start()
+    {
+        _isDiving = heightGoal < heightStart;
+    }
+
+    void Update()
+    {
+        float newHeight = transform.position.y + GetOffset();
+        //Depends if the goal is to go up or go down
+        float encloseHeight = Mathf.Max(_isDiving ? heightStart : heightGoal, Mathf.Min(newHeight, _isDiving ? heightGoal : heightStart));
+        transform.position = new Vector3(transform.position.x, encloseHeight, transform.position.z);
+
+        if (transform.position.y == heightGoal)
+        {
+            GameManager.Instance.WinGame();
+        }
+    }
+
+    private float GetOffset()
     {
         return offsetHeight * (modifier - 0.5f) * 2f;
     }
