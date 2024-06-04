225cb5e938b7d7e07b24b43b949451ccb2c8909b
B3r4ti0n <arcoucou77@gmail.com>
2024-06-04 11:47:25 +0200

    example scenario script

--- Assets/Scripts/Kenophobia/KenophobiaManager.cs
+++ Assets/Scripts/Kenophobia/KenophobiaManager.cs
@@ -18,4 +18,34 @@ public class KenophobiaManager : MonoBehaviour
     {
         
     }
+
+    public static int SwitchStep(int step,StepGameObjectsList objects, System.Func<bool> actualise)
+    {
+        if (actualise())
+        {
+            Debug.Log(" " + (step < 0 || step >= objects.Steps.Count) + " " + step + " " + objects.Steps.Count);
+            if (step < 0 || step >= objects.Steps.Count)
+            {
+                return step;
+            }
+            
+            foreach (var obj in objects.Steps[step].objects)
+            {
+                Debug.Log(obj.name+" "+step);
+                obj.SetActive(false);
+            }
+
+            Debug.Log(" " + (step + 1 < objects.Steps.Count && objects.Steps[step + 1].objects != null) + " " + (step + 1));
+            if (step + 1 < objects.Steps.Count && objects.Steps[step + 1].objects != null)
+            {
+                foreach (var obj in objects.Steps[step + 1].objects)
+                {
+                    obj.SetActive(true);
+                }
+                
+            }
+            return step++;
+        }
+        return step;
+    }
 }
