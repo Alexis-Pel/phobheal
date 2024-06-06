aab41e42f51e1604a6d1a37c91a06baf2b21b048
Infernaton <lubin.pauly@gmail.com>
2024-06-06 11:04:19 +0200

    Thalassophobia - Basic DifficultyMenu done

--- Assets/Scripts/Thalassophobia/DifficultyManager.cs
+++ Assets/Scripts/Thalassophobia/DifficultyManager.cs
@@ -4,40 +4,47 @@ public class DifficultyManager : MonoBehaviour
 {
     [SerializeField] private bool debug = false;
 
-    [SerializeField] private float playerObjective = -50f;
-    [SerializeField] private float fogEndDistance = 25f;
-    [SerializeField] private float faunaDensity = 1f;
-    [SerializeField] private float speedSpawnFish = 0.5f;
+    [SerializeField] private float playerObjectiveBase = -50f;
+    [SerializeField] private float fogEndDistanceBase = 15f;
+    [SerializeField] private float faunaDensityBase = 1f;
+    [SerializeField] private float speedSpawnFishBase = 0.5f;
 
     [SerializeField] private GoalElevator cage;
     [SerializeField] private FishSpawn seaFloor;
     [SerializeField] private Canvas menu;
 
+    public float PlayerObjective { get; private set; }
+    public float FogEndDistance { get; private set; }
+    public float FaunaDensity { get; private set; }
+
     public bool IsValidate { get; private set; }
 
     private void Start()
     {
+        PlayerObjective = playerObjectiveBase;
+        FogEndDistance = fogEndDistanceBase;
+        FaunaDensity = faunaDensityBase;
         if (debug) ValidDifficulty();
     }
 
-    public void SetPlayerObjective(float playerObjective) => this.playerObjective = playerObjective;
-    public void SetFogDistanceStart(float fogEndDistance) => this.fogEndDistance = fogEndDistance;
-    public void SetFaunaDensity(float faunaDensity) => this.faunaDensity = faunaDensity;
+    public void SetPlayerObjective(float coeff) => this.PlayerObjective = playerObjectiveBase + playerObjectiveBase * coeff;
+    public void SetFogDistanceEnd(float coeff) => this.FogEndDistance = fogEndDistanceBase + fogEndDistanceBase * coeff;
+    public void SetFaunaDensity(float coeff) => this.FaunaDensity = coeff;
 
     public void ValidDifficulty()
     {
         menu.enabled = false;
         #region playerObjective
-        cage.HeightGoal = playerObjective;
-        seaFloor.transform.position = new Vector3(seaFloor.transform.position.x, playerObjective, seaFloor.transform.position.z);
+        cage.HeightGoal = PlayerObjective;
+        seaFloor.transform.position = new Vector3(seaFloor.transform.position.x, PlayerObjective, seaFloor.transform.position.z);
         #endregion
 
         #region fogDistance
-        RenderSettings.fogEndDistance = fogEndDistance;
+        RenderSettings.fogEndDistance = FogEndDistance;
         #endregion
 
         #region FaunaDensity
-        seaFloor.StartSpawn(speedSpawnFish * faunaDensity);
+        seaFloor.StartSpawn(speedSpawnFishBase * FaunaDensity);
         #endregion
 
         IsValidate = true;
