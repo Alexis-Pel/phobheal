b97cf2ed76a99a24ff81f594c14eaaae0a2c303d
B3r4ti0n <arcoucou77@gmail.com>
2024-06-10 16:14:24 +0200

    switch kitchen patch

--- Assets/Scripts/Kenophobia/SwitchScript.cs
+++ Assets/Scripts/Kenophobia/SwitchScript.cs
@@ -18,7 +18,7 @@ public class SwitchScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(gameManager._isElectricMeterOn);
+        //Debug.Log(gameManager._isElectricMeterOn);
         if (gameManager != null && gameManager._isElectricMeterOn)
         {
             _isOn = true;
