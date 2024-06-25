2a847a23cc7c0459aa3ee46bb6359686f30a9b78
Infernaton <lubin.pauly@gmail.com>
2024-06-25 16:02:18 +0200

    remove don't destroy on load the gamemanager
    create conflict when player want to replay levels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,10 +21,7 @@ public class GameManager : MonoBehaviour
     void Awake()
     {
         if (Instance == null) // If there is no instance already
-        {
-            DontDestroyOnLoad(gameObject);
             Instance = this;
-        }
         else if (Instance != this) // If there is already an instance and it's not `this` instance
             Destroy(gameObject); // Destroy the GameObject, this component is attached to
     }
